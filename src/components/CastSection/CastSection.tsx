cab172036485dcb4f957721e28c0774519e6061f
Christen Nguyen <christen.nguyen@gmail.com>
2022-09-06 21:01:04 -0700

    feat: build out cast section

--- src/components/CastSection/CastSection.tsx
+++ src/components/CastSection/CastSection.tsx
@@ -1,22 +1,51 @@
 import styled from "styled-components";
 import { SectionCard } from "../../styles";
 import SectionHeader from "../SectionHeader/SectionHeader";
-
-const StyledHeader = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
+import christenHeadShot from "../../images/christen.jpg";
+import samHeadShot from "../../images/sam.jpg";
 
 const StyledCastInfoContainer = styled.div`
   display: flex;
   flex-direction: row;
+  align-items: center;
 `;
 
 const StyledRow = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
 `;
 
+const StyledHeadshot = styled.img`
+  border-radius: 50%;
+  margin-right: 12px;
+  height: 96px;
+  cursor: pointer;
+  :hover {
+    filter: brightness(85%);
+  }
+`;
+
+const StyledCastText = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledActorName = styled.div`
+  font-weight: bold;
+  cursor: pointer;
+  :hover {
+    color: grey;
+  }
+`;
+
+const StyledRole = styled.div`
+  color: #808080;
+  cursor: pointer;
+  :hover {
+    text-decoration: underline;
+  }
+`;
 interface ICastInfoType {
   imageUrl: string;
   name: string;
@@ -25,10 +54,11 @@ interface ICastInfoType {
 const CastInfo: React.FC<ICastInfoType> = ({ imageUrl, name, role }) => {
   return (
     <StyledCastInfoContainer>
-      <div>{imageUrl}</div>
-      <div>
-        {name} {role}
-      </div>
+      <StyledHeadshot src={imageUrl} alt={name} />
+      <StyledCastText>
+        <StyledActorName>{name}</StyledActorName>
+        <StyledRole>{role}</StyledRole>
+      </StyledCastText>
     </StyledCastInfoContainer>
   );
 };
@@ -38,8 +68,12 @@ const CastSection = () => {
     <SectionCard>
       <SectionHeader text="Top Cast" />
       <StyledRow>
-        <CastInfo imageUrl="poop" name="Sam Mathison" role="Boyf" />
-        <CastInfo imageUrl="poop" name="Christen Nguyen" role="Girlf" />
+        <CastInfo imageUrl={samHeadShot} name="Sam Mathison" role="Boyf" />
+        <CastInfo
+          imageUrl={christenHeadShot}
+          name="Christen Nguyen"
+          role="Girlf"
+        />
       </StyledRow>
     </SectionCard>
   );
